ffa6cc5eb270048ce17cc204a9831f166a07d78f
Duggan Roberts <droberts@quailcreekbank.com>
2016-03-01 13:22:03 -0600

    order by date

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -4,7 +4,7 @@ class TournamentsController < ApplicationController
   # GET /tournaments
   # GET /tournaments.json
   def index
-    @tournaments = Tournament.all
+    @tournaments = Tournament.order("date")
   end
 
   # GET /tournaments/1
